22ec36526dd9e27d73437e9a06349a9799ebe7c2
piyushhhxyz <piyush.bhawsar1@gmail.com>
2024-05-05 02:29:24 +0530

    vine auth basic added

--- validations/auth.validate.js
+++ validations/auth.validate.js
@@ -1,8 +1,12 @@
 import vine from '@vinejs/vine'
 import { CustomErrorReporter } from './CustomError.reporter.js'
 
+//custom Error reportter
+vine.errorReporter = () => new CustomErrorReporter(); 
+
 export const registerSchema = vine.object({
     name: vine.string(),
     email: vine.string().email(),
     password: vine.string().minLength(6).maxLength(32).confirmed()
 })
+
